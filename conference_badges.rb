909d6aad8e38412de8d368e32cf0dd153171b17b
Joe Jamsky <cba1067950@yahoo.com>
2019-04-25 22:00:19 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,7 +6,7 @@ end
 def batch_badge_creator(names)
   messageArray = []
   names.each do |item|
-    messageArray.pop("Hello, my name is #{item}")
+    messageArray.pop("Hello, my name is #{item}.")
   end
   
   messageArray
